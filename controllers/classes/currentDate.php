4064cdadb33db3b627b6ad169d059e12b7605c3f
Kacper Koziol <kacperu770@gmail.com>
2019-10-29 20:05:07 +0100

    Separated getDate method to getMonth, getDay, getDate. Need to translating

--- controllers/classes/currentDate.php
+++ controllers/classes/currentDate.php
@@ -1,14 +1,21 @@
 <?php
 
 class CurrentDate{
-    private $date;
+    private $day;
+    private $month;
 
-    public function getDate(){
-
-        $month = date("F", strtotime("m"));
-        $day = date("d");
-        $this->date = $month . " " . $day;
-        return $this->date;
+    public function getDay(){
+        $this->day = date("d");
+        return $this->day;
     }
 
+    public function getMonth(){
+        $this->month = date("F", strtotime("m"));
+        return $this->month;
+    }
+
+    public function getDate($month){
+        $date = $month . ", " . $this->day;
+        return $date;
+    }
 }
